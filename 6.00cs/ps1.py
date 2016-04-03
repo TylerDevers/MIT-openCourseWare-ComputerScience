34f0ca8f780aa6a92594938a182dda36b6324703
Tyler <devers.tyler@gmail.com>
2016-04-02 20:57:43 -0700

    completed test case 1 on ps_02

--- 6.00cs/ps1.py
+++ 6.00cs/ps1.py
@@ -1,9 +1,20 @@
 
-
-outstandingBal = float(raw_input("How much do you owe on the loan? $" ))
+#Test case 1 - calculate the principal owed after a year if only the minimum payments were made on a credit card.
+print("Lets calculate the amount owed on a credit card loan at the end of the year, when only the minimum payment is paid.")
+print("")
+outstandingBal = int(raw_input("How much do you owe on the loan? $" ))
 apr = float(raw_input("what is the annual percentage rate as a decimal? " ))
 monthlyPaymentRate = float(raw_input("what is the minimum monthly payment rate as a decimal? "))
 
 for month in range(1,13):
-    print month
-    
+    print ("Month : " + str(month))
+    minPayment = monthlyPaymentRate * outstandingBal
+    intPaid = (apr/12) * outstandingBal
+    principalPaid = minPayment - intPaid
+    remainBal = outstandingBal - principalPaid
+    outstandingBal = remainBal
+    print ("Minimum monthly payment is : $" + str(round(minPayment,2)))
+    print ("Principal paid is : $" + str(round(principalPaid,2)))
+    print ("Remaining balance is : $" + str(round(outstandingBal,2)))
+    month += 1
+    print("")
