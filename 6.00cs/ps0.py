8b714cacb73d2f59d24e89e64815f2e1c3c52248
Tyler <devers.tyler@gmail.com>
2016-04-02 20:08:53 -0700

    first commit

---
+++ 6.00cs/ps0.py
@@ -0,0 +1,5 @@
+
+#asks for date of birth and name.
+dob = raw_input('Please tell me your date of birth ')
+name = raw_input('Please tell me your name ')
+print('Your name is ' + name + ' and your birth date is ' + dob)
---
+++ 6.00cs/ps1.py
@@ -0,0 +1,9 @@
+
+
+outstandingBal = float(raw_input("How much do you owe on the loan? $" ))
+apr = float(raw_input("what is the annual percentage rate as a decimal? " ))
+monthlyPaymentRate = float(raw_input("what is the minimum monthly payment rate as a decimal? "))
+
+for month in range(1,13):
+    print month
+    
