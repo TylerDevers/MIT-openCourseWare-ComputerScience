1e572b34327e8e3f162e896f07dead3ad2910bf9
Tyler <devers.tyler@gmail.com>
2016-04-03 10:33:05 -0700

    refactored ps0.py

--- 6.00cs/ps0.py
+++ 6.00cs/ps0.py
@@ -1,5 +1,9 @@
 
 #asks for date of birth and name.
-dob = raw_input('Please tell me your date of birth ')
-name = raw_input('Please tell me your name ')
-print('Your name is ' + name + ' and your birth date is ' + dob)
+# Write a program that does the following in order:
+# Asks the user to enter his/her date of birth.
+# Asks the user to enter his/her last name.
+# Prints out the user’s last name and date of birth, in that order.
+dob = raw_input('Please tell me your date of birth : ')
+name = raw_input('Please tell me your last name : ')
+print('Your last name is ' + name + ' and your birth date is ' + dob)
