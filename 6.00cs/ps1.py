eb1d4f851db041492be39b7ef24e070462a30bd5
Tyler <devers.tyler@gmail.com>
2016-04-04 17:01:36 -0700

    began problem 3 ps1.py, using bisection

--- 6.00cs/ps1.py
+++ 6.00cs/ps1.py
@@ -23,33 +23,55 @@
 # Now write a program that calculates the minimum fixed monthly payment needed in order pay
 # off a credit card balance within 12 months. We will not be dealing with a minimum monthly
 # payment rate.
-initialBalance = float(raw_input("Enter the outstanding balance on your credit card: "))
-interestRate = float(raw_input("Enter the annual credit card interest rate as a decimal: "))
+# initialBalance = float(raw_input("Enter the outstanding balance on your credit card: "))
+# interestRate = float(raw_input("Enter the annual credit card interest rate as a decimal: "))
+#
+# # Initialize state variables. state variables are tbe most basic variables that describe the system.
+# monthlyPayment = 0
+# monthlyInterestRate = interestRate/12
+# balance = initialBalance
+#
+# # Test increasing amounts of monthlyPayment in increments of $100
+# # until it can be paid off in a year
+# #use nested while loops. the second loop will run as long as the first loops parameters are not met, which is balance being less that zero. If the second loop cannot bring the balance down to zero, the second loop will exit and the first loop will increase the monthly payment by ten.
+# while balance > 0:
+#
+#     monthlyPayment += 10
+#     balance = initialBalance
+#     numMonths = 0
+#
+#     while numMonths < 12 and balance > 0: #this is why balance can become negative.
+#
+#         # Count this as a new month
+#         numMonths += 1
+#         interest = monthlyInterestRate * balance
+#         balance -= monthlyPayment
+#         balance += interest
+#
+# balance = round(balance,2)
+#
+# print "Monthly payment to pay off debt in 1 year:", monthlyPayment
+# print "Number of months needed:", numMonths
+# print "Balance:",balance
 
-# Initialize state variables. state variables are tbe most basic variables that describe the system.
-monthlyPayment = 0
-monthlyInterestRate = interestRate/12
-balance = initialBalance
+#problem 3  -  use biesection search
+#get input
+initialBal = float(raw_input("What is the outstanding balance? : $"))
+annualRate = float(raw_input("what is the annual percentage rate in decimal? :"))
 
-# Test increasing amounts of monthlyPayment in increments of $100
-# until it can be paid off in a year
-#use nested while loops. the second loop will run as long as the first loops parameters are not met, which is balance being less that zero. If the second loop cannot bring the balance down to zero, the second loop will exit and the first loop will increase the monthly payment by ten.
-while balance > 0:
+#Initialize
+balance = initialBal
+lowerBound = balance / 12.0
+upperBound = (balance * (1 + (annualRate / 12.0)) ** 12.0) / 12.0
 
-    monthlyPayment += 10
-    balance = initialBalance
-    numMonths = 0
+while True:
+    balance = initialBal
+    monthlyPayment = (lowerBound + upperBound)/2
 
-    while numMonths < 12 and balance > 0: #this is why balance can become negative.
-
-        # Count this as a new month
-        numMonths += 1
-        interest = monthlyInterestRate * balance
-        balance -= monthlyPayment
-        balance += interest
-
-balance = round(balance,2)
-
-print "Monthly payment to pay off debt in 1 year:", monthlyPayment
-print "Number of months needed:", numMonths
-print "Balance:",balance
+    for month in range (1,13):
+        interest = balance * (annualRate/12)
+        balance += interest - monthlyPayment
+        balance < 0
+        break
+    if balance < 0
+        upperbound = 
