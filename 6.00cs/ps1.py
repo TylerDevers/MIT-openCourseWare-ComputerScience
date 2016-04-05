0b8fa9c9ce638f8e8d188c1eee14d49078c4802c
Tyler <devers.tyler@gmail.com>
2016-04-04 19:29:09 -0700

    added solution for problem 3 to ps1.py

--- 6.00cs/ps1.py
+++ 6.00cs/ps1.py
@@ -56,22 +56,49 @@
 
 #problem 3  -  use biesection search
 #get input
-initialBal = float(raw_input("What is the outstanding balance? : $"))
-annualRate = float(raw_input("what is the annual percentage rate in decimal? :"))
+original_balance = float(raw_input("Enter the outstanding balance on your credit card: "))
+interest_rate = float(raw_input("Enter the annual credit card interest rate as a decimal: "))
 
-#Initialize
-balance = initialBal
-lowerBound = balance / 12.0
-upperBound = (balance * (1 + (annualRate / 12.0)) ** 12.0) / 12.0
+# Initialize state variables
+balance = original_balance
+low_payment = balance/12
+high_payment = (balance*(1+(interest_rate/12))**12)/12
 
+# Use bisection search until the search space is sufficiently small
 while True:
-    balance = initialBal
-    monthlyPayment = (lowerBound + upperBound)/2
+    balance = original_balance
+    monthly_payment = (low_payment + high_payment)/2
 
-    for month in range (1,13):
-        interest = balance * (annualRate/12)
-        balance += interest - monthlyPayment
-        balance < 0
+    # Simulate passage of time until outstanding balance is paid off
+    # Each iteration represents 1 month
+    for month in range(1,13):
+        interest = round(balance*interest_rate/12, 2)
+        balance += interest - monthly_payment
+        if balance <= 0:
+            break
+
+    if (high_payment - low_payment < 0.005):
+        # Bisection search space is small enough
+        # Print result
+        print "RESULT"
+
+        # Round monthly payment up to the nearest cent
+        monthly_payment = round(monthly_payment + 0.004999, 2)
+        print "Monthly payment to pay off debt in 1 year:", round(monthly_payment,2)
+
+        # Recompute remaining balance and the number of months needed
+        balance = original_balance
+        for month in range(1,13):
+            interest = round(balance*interest_rate/12, 2)
+            balance += interest - monthly_payment
+            if balance <= 0:
+                break
+        print "Number of months needed:", month
+        print "Balance:", round(balance,2)
         break
-    if balance < 0
-        upperbound = 
+    elif balance < 0:
+        #Paying too much
+        high_payment = monthly_payment
+    else:
+        #Paying too little
+        low_payment = monthly_payment
