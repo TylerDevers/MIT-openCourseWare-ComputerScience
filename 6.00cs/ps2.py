963877a75df1b8aa19efd43283dd1240eabd6a4a
Tyler <devers.tyler@gmail.com>
2016-04-08 10:35:38 -0700

    began solution for problem 1, problem set 2 - polynomials

---
+++ 6.00cs/ps2.py
@@ -0,0 +1,11 @@
+# Problem #1
+# Implement the evaluate_poly function. This function evaluates a polynomial function for the
+# given x value. It takes in a tuple of numbers poly and a number x. By number, we mean
+# that x and each element of poly is a float. evaluate_poly takes the polynomial represented
+# by poly and computes its value at x. It returns this value as a float.
+
+#
+def eval_poly (poly):
+    print poly.index(poly)
+    
+eval_poly(1)
