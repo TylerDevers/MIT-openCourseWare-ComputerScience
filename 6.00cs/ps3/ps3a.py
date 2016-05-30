616036e4f61c31be10a320c3127c6f41df28e4e9
TylerDevers <devers.tyler@gmail.com>
2016-05-29 21:12:40 -0700

    completed problem 5 of ps3

--- 6.00cs/ps3/ps3a.py
+++ 6.00cs/ps3/ps3a.py
@@ -261,6 +261,20 @@ def play_game(word_list):
     * If the user inputs anything else, ask them again.
     """
     # TO DO...
+    hand = deal_hand(HAND_SIZE) # random init
+    while True:
+        cmd = raw_input('Enter n to deal a new hand, r to replay the last hand, or e to end game: ')
+        if cmd == 'n':
+            hand = deal_hand(HAND_SIZE)
+            play_hand(hand.copy(), word_list)
+            print
+        elif cmd == 'r':
+            play_hand(hand.copy(), word_list)
+            print
+        elif cmd == 'e':
+            break
+        else:
+            print "Invalid command."
 
 #
 # Build data structures used for entire session and play game
